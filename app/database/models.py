789a4fd06ca7d298864f0c815c3e8446acffa6ee
William <williamcipriano344@gmail.com>
2026-07-07 20:36:39 -0300

    feat: configura banco de dados com SQLite e SQLAlchemy

---
+++ app/database/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Boolean, Column, Integer, String
+
+from app.database.database import Base
+
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    description = Column(String, nullable=True)
+    completed = Column(Boolean, default=False)
--- app/main.py
+++ app/main.py
@@ -1,8 +1,15 @@
-# app/main.py
-
 from fastapi import FastAPI
 
-app = FastAPI()
+from app.database.database import Base, engine
+from app.database.models import Task
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(
+    title="Task Manager API",
+    description="API REST para gerenciamento de tarefas.",
+    version="1.0.0"
+)
 
 
 @app.get("/")
